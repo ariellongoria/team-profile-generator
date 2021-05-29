dd5ab2934c5e424536526afbc05d822ba1ad541b
ariellongoria <ariello97@hotmail.com>
2021-05-29 17:20:40 -0500

    Engineer class and tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,22 @@
 const Engineer = require("../lib/Engineer");
+
+test("creates an engineer object", () => {
+    const engineer = new Engineer("Bob", 1, "bob@work.com", "bobcodeswell");
+
+    expect(engineer.name).toBe("Bob");
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe("bob@work.com");
+    expect(engineer.github).toBe("bobcodeswell");
+});
+
+test("gets engineer github", () => {
+    const engineer = new Engineer("Bob", 1, "bob@work.com", "bobcodeswell");
+
+    expect(engineer.getGithub()).toBe("bobcodeswell");
+});
+
+test("gets engineer role", () => {
+    const engineer = new Engineer("Bob", 1, "bob@work.com", "bobcodeswell");
+
+    expect(engineer.getRole()).toBe("Engineer");
+});
